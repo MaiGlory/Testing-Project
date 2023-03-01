258df44e4034b4bbcf07fe63dafbe06b13dde0f3
Mai Glory <moonn2932@gmail.com>
2023-03-01 09:29:16 +0630

    Add new Animal Project

---
+++ src/AnimalShow.js
@@ -0,0 +1,39 @@
+import './AnimalShow.css';
+import { useState } from 'react';
+
+import bird from './svg/bird.svg';
+import cat from './svg/cat.svg';
+import cow from './svg/cow.svg';
+import dog from './svg/dog.svg';
+import gator from './svg/gator.svg';
+import heart from './svg/heart.svg';
+import horse from './svg/horse.svg';
+
+const svgMap = {
+    bird, //if key and value are the same don't need to write bird:bird,
+    cat, // equlvalent to cat: cat,
+    cow,
+    dog,
+    gator,
+    heart,
+    horse
+};
+
+function AnimalShow({type}){
+
+    const [click, setClick] = useState(0);
+
+    const handleClick = () => {
+        setClick(click +1);
+    };
+
+    return(
+        <div className='animal-show' onClick={handleClick}>
+            <img className='animal' alt="animal" src={svgMap[type]} />
+            <img className='heart' alt="heart" src={heart} style={{width: 10 + 10 * click + 'px'}}
+            />
+        </div>
+    );
+}
+
+export default AnimalShow;
